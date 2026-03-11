4b9573f43dd411f3f18af7a2fb8a9fd13aa677bc
Cuncis <38809651+Cuncis@users.noreply.github.com>
2026-03-11 21:04:50 +0700

    Add task list functionality to welcome page

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ Route::get('/', function () {
     return view('welcome', [
         'greeting' => 'Helloo, ',
         'name' => request('name', 'Guest'),
+        'tasks' => ['Go to the store', 'Go to the bank', 'Go to the post office'],
     ]);
 });
 
