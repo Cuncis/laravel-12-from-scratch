77d537d2ad187d821c24d3753c0ee9f2202c1cd7
Cuncis <38809651+Cuncis@users.noreply.github.com>
2026-03-29 17:15:34 +0700

    Refactor routing and middleware for ideas management; add foreign key constraint for user in ideas table

--- routes/web.php
+++ routes/web.php
@@ -8,18 +8,24 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [IdeaController::class, 'index']);
-Route::get('/ideas', [IdeaController::class, 'index']);
-Route::get('/ideas/create', [IdeaController::class, 'create']);
-Route::post('/ideas', [IdeaController::class, 'store']);
-Route::get('/ideas/{idea}', [IdeaController::class, 'show']);
-Route::get('/ideas/{idea}/edit', [IdeaController::class, 'edit']);
-Route::patch('/ideas/{idea}', [IdeaController::class, 'update']);
-Route::delete('/ideas/{idea}', [IdeaController::class, 'destroy']);
 
-Route::get('/register', [RequestedUserController::class, 'create']);
-Route::post('/register', [RequestedUserController::class, 'store']);
+Route::middleware('auth')->group(function () {
+    Route::get('/', fn() => redirect('/ideas'));
+    Route::get('/ideas', [IdeaController::class, 'index']);
+    Route::get('/ideas/create', [IdeaController::class, 'create']);
+    Route::post('/ideas', [IdeaController::class, 'store']);
+    Route::get('/ideas/{idea}', [IdeaController::class, 'show']);
+    Route::get('/ideas/{idea}/edit', [IdeaController::class, 'edit']);
+    Route::patch('/ideas/{idea}', [IdeaController::class, 'update']);
+    Route::delete('/ideas/{idea}', [IdeaController::class, 'destroy']);
 
-Route::get('/login', [SessionsController::class, 'create']);
-Route::post('/login', [SessionsController::class, 'store']);
-Route::delete('/logout', [SessionsController::class, 'destroy']);
+    Route::delete('/logout', [SessionsController::class, 'destroy']);
+});
+
+Route::middleware('guest')->group(function () {
+    Route::get('/register', [RequestedUserController::class, 'create']);
+    Route::post('/register', [RequestedUserController::class, 'store']);
+
+    Route::get('/login', [SessionsController::class, 'create'])->name('login');
+    Route::post('/login', [SessionsController::class, 'store']);
+});
