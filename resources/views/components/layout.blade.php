7fe0ff0e73571bf7f91948b8a1f5ad8285282784
Cuncis <38809651+Cuncis@users.noreply.github.com>
2026-04-02 18:11:35 +0700

    feat: add daisyui as a dependency and configure themes in app.css

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -8,12 +8,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{ $title }}</title>
-    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
-    <link href="https://cdn.jsdelivr.net/npm/daisyui@5" rel="stylesheet" type="text/css" />
-    <link href="https://cdn.jsdelivr.net/npm/daisyui@5/themes.css" rel="stylesheet" type="text/css" />
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
-<body >
+<body>
     <x-nav />
     <main class="max-w-2xl mx-auto mt-10">
         {{ $slot }}
